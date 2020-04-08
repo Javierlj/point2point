25db538cfa49169d383e5e1e693a83181cd8261e
Carlos Arriaga <carlos.arriaga.prieto@alumnos.upm.es>
2020-04-08 18:03:10 +0200

    (feat) Lime Petition

--- src/servlets/FormPetitionServlet.java
+++ src/servlets/FormPetitionServlet.java
@@ -25,10 +25,12 @@ public class FormPetitionServlet extends HttpServlet{
 	
 	public FormPetitionServlet() throws MalformedURLException {
 		System.out.print("url");
-		serviceUrl = new URL("http://global-n-mobike-g.mobike.com/api/nearby/v4/nearbyBikeInfo");
+		final URL MobikeUrl = new URL("http://global-n-mobike-g.mobike.com/api/nearby/v4/nearbyBikeInfo");
+		final URL HiveUrl = new URL("https://hive.frontend.fleetbird.eu/api/prod/v1.06/map/cars");
+		urls = new URL[]{MobikeUrl, HiveUrl};
 	}
 	
-	private URL serviceUrl;
+	private URL[] urls;	
 
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -37,29 +39,34 @@ public class FormPetitionServlet extends HttpServlet{
 			String latitude = req.getParameter("latitude");
 			String longitude = req.getParameter("longitude");
 			String radio = req.getParameter("radio");
-			String urlParameters="latitude="+latitude+"&longitude="+longitude+"&scope="+radio; 
-			System.out.print(urlParameters);
-			HttpURLConnection myURLConnection = (HttpURLConnection)serviceUrl.openConnection();
-			byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
-			int    postDataLength = postData.length;
-			myURLConnection.setRequestMethod("POST");
-			myURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-			myURLConnection.setRequestProperty("platform", "1");
-			myURLConnection.setRequestProperty("Content-Language", "en-US");
-			myURLConnection.setRequestProperty("User-Agent","Mozilla/5.0 (Android 7.1.2; Pixel Build/NHG47Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.9 NTENTBrowser/3.7.0.496 (IWireless-US) Mobile Safari/537.36"); 
-			myURLConnection.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
-			myURLConnection.setUseCaches(false);
-			myURLConnection.setDoInput(true);
-			myURLConnection.setDoOutput(true);	
-			try( DataOutputStream wr = new DataOutputStream( myURLConnection.getOutputStream())) {
-				   wr.write( postData );
-				}
-			Reader in = new BufferedReader(new InputStreamReader(myURLConnection.getInputStream(), "UTF-8"));
-			StringBuilder sb = new StringBuilder();
-		    for (int c; (c = in.read()) >= 0;)
-		        sb.append((char)c);
-		    String response = sb.toString();
-		    System.out.println(response);
+			String mobikeParameters="latitude="+latitude+"&longitude="+longitude+"&scope="+radio; 
+			String HiveParameters="latitude="+latitude+"&longitude="+longitude;
+			String[] parameters = new String[] {mobikeParameters,HiveParameters}; 
+			for(int i = 0 ; i<urls.length;i++) {
+				URL serviceUrl = urls[i];
+				String urlParameters = parameters[i];
+				HttpURLConnection myURLConnection = (HttpURLConnection)serviceUrl.openConnection();
+				byte[] postData       = urlParameters.getBytes( StandardCharsets.UTF_8 );
+				int    postDataLength = postData.length;
+				myURLConnection.setRequestMethod("POST");
+				myURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+				myURLConnection.setRequestProperty("platform", "1");
+				myURLConnection.setRequestProperty("Content-Language", "en-US");
+				myURLConnection.setRequestProperty("User-Agent","Mozilla/5.0 (Android 7.1.2; Pixel Build/NHG47Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.9 NTENTBrowser/3.7.0.496 (IWireless-US) Mobile Safari/537.36"); 
+				myURLConnection.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
+				myURLConnection.setUseCaches(false);
+				myURLConnection.setDoInput(true);
+				myURLConnection.setDoOutput(true);	
+				try( DataOutputStream wr = new DataOutputStream( myURLConnection.getOutputStream())) {
+					   wr.write( postData );
+					}
+				Reader in = new BufferedReader(new InputStreamReader(myURLConnection.getInputStream(), "UTF-8"));
+				StringBuilder sb = new StringBuilder();
+			    for (int c; (c = in.read()) >= 0;)
+			        sb.append((char)c);
+			    String response = sb.toString();
+			    System.out.println(response);
+			}
 
 		} catch (ProtocolException e) {
 			e.printStackTrace();
